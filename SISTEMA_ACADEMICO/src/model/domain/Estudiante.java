9f787483d1f10051597d895a547a8c2464edd84b
Juan camilo Osorio <camilosorio.2016@gmail.com>
2026-09-07 22:56:06 -0500

    Crear clase Estudiantes

--- SISTEMA_ACADEMICO/src/model/domain/Estudiante.java
+++ SISTEMA_ACADEMICO/src/model/domain/Estudiante.java
@@ -1,7 +1,34 @@
 package model.domain;
 
-public class Estudiante {
+public class Estudiante extends Persona{
     private String codigo;
     private int semestreActual;
+
+    public Estudiante(String identificacion, String nombre, String correo, String codigo, int semestreActual){
+        super(identificacion, nombre, correo);
+        this.codigo = codigo;
+        this.semestreActual = semestreActual;
+    }
+    
+    @Override 
+    public String identificarRol() {
+        return "Estudiante";
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getSemestreActual() {
+        return semestreActual;
+    }
+
+    public void setSemestreActual(int semestreActual) {
+        this.semestreActual = semestreActual;
+    }
     
 }
