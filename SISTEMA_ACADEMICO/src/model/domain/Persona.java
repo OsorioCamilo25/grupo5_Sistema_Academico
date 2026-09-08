55a91ae26f90e32c251efff68299476c4dd126f5
Juan camilo Osorio <camilosorio.2016@gmail.com>
2026-09-07 22:24:56 -0500

    Crear clase abstracta Persona

---
+++ SISTEMA_ACADEMICO/src/model/domain/Persona.java
@@ -0,0 +1,31 @@
+package model.domain;
+
+public abstract class Persona {
+    private String identificacion;
+    private String nombre;
+    private String correo; 
+
+    public Persona(String identificacion, String nombre, String correo){
+        this.identificacion = identificacion;
+        this.nombre = nombre;
+        this.correo = correo;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+    
+    @Override
+    public String datosResumen(){
+        return "Identificación: " + identificacion + ", Nombre: " + nombre + ", Correo: " + correo;
+    } 
+
+}
